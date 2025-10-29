453409fd89698e2c99cc2016572c945220144b21
Sergiu Gherasim <sergeras4@gmail.com>
2025-10-29 12:49:04 +0200

    feat: Experiment with the created class by printing, testing, and modifying it

--- Main.java
+++ Main.java
@@ -1,7 +1,16 @@
 public class Main {
   public static void main(String[] args) {
-    Simple s = new Simple();
+    String[] physicalTraits = {"BLONDE"};
+    Individual ind = new Individual(0, true, "KASHYYYK", 20, physicalTraits);
 
-    s.printHello();
+    System.out.println("ID: " + ind.getId());
+    System.out.println("IsHumanoid: " + ind.getIsHumanoid());
+    System.out.println("Planet of Origin: " +  ind.getOriginPlanet());
+    System.out.println("Age: " + ind.getAge());
+
+    System.out.println("Physical traits:");
+    for (int i = 0; i < ind.getPhysicalTraits().length; i++) {
+      System.out.println(" - " + physicalTraits[i]);
+    }
   }
 }
