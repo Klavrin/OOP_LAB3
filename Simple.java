46fcf1a369b408c8b4ae23859877e98c5c06beaf
Sergiu Gherasim <sergeras4@gmail.com>
2025-10-29 12:31:50 +0200

    feat: create a simple class

---
+++ Simple.java
@@ -0,0 +1,11 @@
+public class Simple {
+  private String hello = "Hello world!";
+
+  public String getHello() {
+    return hello;
+  }
+
+  public void printHello() {
+    System.out.println(hello);
+  }
+}
