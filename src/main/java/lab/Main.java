177e65931a19bd9f5584eb09663b680e8676f5e9
Sergiu Gherasim <sergeras4@gmail.com>
2025-10-30 00:43:24 +0200

    feat: filter through individuals

--- src/main/java/lab/Main.java
+++ src/main/java/lab/Main.java
@@ -1,21 +1,65 @@
 package lab;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 public class Main {
   public static void main(String[] args) {
-    String[] physicalTraits = {"BLONDE"};
-    Individual ind = new Individual(0, true, "KASHYYYK", 20, physicalTraits);
+    Gson gson = new Gson();
 
-    System.out.println("ID: " + ind.getId());
-    System.out.println("IsHumanoid: " + ind.getIsHumanoid());
-    System.out.println("Planet of Origin: " +  ind.getOriginPlanet());
-    System.out.println("Age: " + ind.getAge());
+    try {
+      String json = Files.readString(Paths.get("input.json"));
+      TypeToken<List<Individual>> listType = new TypeToken<List<Individual>>() {};
+      List<Individual> individuals = gson.fromJson(json, listType.getType());
 
-    System.out.println("Physical traits:");
-    for (int i = 0; i < ind.getPhysicalTraits().length; i++) {
-      System.out.println(" - " + physicalTraits[i]);
+      List<Individual> starWarsSpecies = new ArrayList<>();
+      List<Individual> marvelSpecies = new ArrayList<>();
+      List<Individual> hitchhikersSpecies = new ArrayList<>();
+      List<Individual> lordOfTheRingsSpecies = new ArrayList<>();
+
+      for (Individual ind : individuals) {
+        if (ind.getPhysicalTraits() == null)
+          continue;
+
+        Species classification = ind.classify();
+        if (classification == null)
+          continue;
+
+        switch (classification) {
+          case WOOKIE:
+          case EWOK:
+            starWarsSpecies.add(ind);
+            break;
+
+          case ASGARDIAN:
+            marvelSpecies.add(ind);
+            break;
+
+          case BETELGEUSIAN:
+          case VOGON:
+            hitchhikersSpecies.add(ind);
+            break;
+
+          case ELF:
+          case DWARF:
+            lordOfTheRingsSpecies.add(ind);
+            break;
+        }
+      }
+
+      WriteFile wf = new WriteFile("src/main/java/lab");
+      wf.write(starWarsSpecies, "starwars");
+      wf.write(marvelSpecies, "marvel");
+      wf.write(hitchhikersSpecies, "hitchhiker");
+      wf.write(lordOfTheRingsSpecies, "rings");
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-
-    ReadFile rf = new ReadFile("input.json");
-    rf.printFile();
   }
 }
